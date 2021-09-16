6ea09ba84725d3e342d2c3f485de0943df2e547a
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-16 13:32:52 +0530

    feat : api tested

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const fetch = require('node-fetch')
+const englishText = document.querySelector('#user-input')
+const minionTranslate = document.querySelector('#minion-translate')
+const minionOutput = document.querySelector('#minion-output')
+const q = document.querySelector('#q')
+const p = document.querySelector('#heading')
 
-const serverUrl = 'https://api.funtranslations.com/translate/minion.json?'
-// console.log(`${serverUrl}text="i am "`)
-fetch(`${serverUrl}text="i am "`)
-  .then(response => response.json())
-  .then(result => {
-    console.log('Success:', result)
-  })
-  .catch(error => {
-    console.error('Error:', error)
-  })
+const serverUrl = 'https://api.funtranslations.com/translate/minion.json'
+
+function createFinalUrl (serverUrl) {
+  userInput = englishText.value
+  return `${serverUrl}?text=${userInput}`
+}
+
+function takeInput () {
+  userInput = englishText.value
+  // console.log(createFinalUrl(serverUrl))
+  fetch(createFinalUrl(serverUrl))
+    .then(response => response.json())
+    .then(result => {
+      console.log(result.contents.translation)
+      console.log(result)
+      p.innerText = result.contents.translated
+    })
+    .catch(error => {
+      console.log('object')
+      console.error('Error:', error)
+    })
+}
+
+minionTranslate.addEventListener('click', takeInput)
